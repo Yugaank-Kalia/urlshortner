d66e970be6e10dacc80e913a6c5f6b689c9670de
Yugaank Kalia <yugaank@outlook.com>
2019-10-07 01:08:13 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+// Connect Database
+connectDB();
+
+app.use(express.json({extended : false}))
+
+// define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000
+app.listen(PORT, ()=> console.log(`app running on port ${PORT}`))
